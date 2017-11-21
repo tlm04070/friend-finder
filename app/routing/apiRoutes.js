3ea6eebc747e3c313bf0be3938cae7f6b4a3481e
Trevor Meadows <tlm04070@gmail.com>
2017-11-20 22:40:06 -0500

    working loop inside of other loop

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var personData = require('../data/friend.js');
+var personData = require('../data/friend');
 
 module.exports = function (app) {
 
@@ -8,8 +8,26 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         var newPerson = req.body;
-        console.log(newPerson);
         personData.push(newPerson);
+        var currentScore = newPerson.scores;
+        // console.log(currentScore);
+        console.log(personData);
 
+        var bestMatch = function () {
+
+            for (let i = 0; i < personData.length - 1; i++) {
+                var currentFriendData = personData[i];
+                console.log(currentFriendData);
+                for (let j = 0; j < currentFriendData.scores.length; j++) {
+                    var currentFriendScore = currentFriendData.scores[j];
+                    console.log("score: " + currentFriendScore);
+                }
+            }
+        }
+        bestMatch();
+
+
+
+        res.json(personData);
     });
 };
