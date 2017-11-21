1dc62431cefb25408fbea4e13b7313c8b6db556c
Trevor Meadows <tlm04070@gmail.com>
2017-11-20 21:38:40 -0500

    got object to pass an array as the value

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ var PORT = process.env.PORT || 3005;
 
 
 app.use(bodyParser.urlencoded({
-    extended: false
+    extended: true
 }));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+}));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
