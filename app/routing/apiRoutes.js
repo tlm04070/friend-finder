3567ff1e5e9503cb3d2706980cee3aab2c68af94
Trevor Meadows <tlm04070@gmail.com>
2017-11-23 20:20:33 -0500

    figuring out the bubble sort to .pop() lowest difference

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,74 +1,103 @@
-var personData = require('../data/friend');
+var personData = require("../data/friend");
 
-module.exports = function (app) {
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(personData);
+  });
 
-    app.get('/api/friends', function (req, res) {
-        res.json(personData);
-    });
+  app.post("/api/friends", function(req, res) {
+    var newPerson = req.body;
+    personData.push(newPerson);
 
-    app.post("/api/friends", function (req, res) {
-        var newPerson = req.body;
-        personData.push(newPerson);
+    var currentScore = newPerson.scores;
+    var currentPhoto = newPerson.photo;
 
-        var currentScore = newPerson.scores;
-        var currentPhoto = newPerson.photo;
+    let inputScore = [];
+    let sumTotal = [];
+    let personArray = [];
+    let subArray = [];
+    for (let i = 0; i < currentScore.length; i++) {
+      var newScore = parseInt(currentScore[i]);
+      inputScore.push(newScore);
+    }
+    var inputSum = inputScore.reduce((a, b) => a + b, 0);
+    // console.log(inputSum);
+    var bestMatch = function() {
+      for (let i = 0; i < personData.length - 1; i++) {
+        var currentFriendData = personData[i];
+        //console.log("last: " + currentFriendData.scores);
+        var friendPhoto = currentFriendData.photo;
+        //console.log("photo :" + friendPhoto);
+        var differenceArr = [];
 
-        let inputScore = [];
-        let sumTotal = [];
-        let personArray = [];
-        for (let i = 0; i < currentScore.length; i++) {
-            var newScore = parseInt(currentScore[i]);
-            inputScore.push(newScore);
+        for (let j = 0; j < currentFriendData.scores.length; j++) {
+          var currentFriendScore = currentFriendData.scores[j];
+          //   console.log("score: " + currentFriendScore);
+          //   console.log("new person score: " + currentScore[j]);
+          //   console.log(
+          //     "difference: " +
+          //       Math.abs(parseInt(currentFriendScore) - parseInt(currentScore[j]))
+          //   );
+          var difference = Math.abs(
+            parseInt(currentFriendScore) - parseInt(currentScore[j])
+          );
+          differenceArr.push(difference);
         }
-        var inputSum = inputScore.reduce((a, b) => a + b, 0);
-        console.log(inputSum);
-        var bestMatch = function () {
+        //console.log(differenceArr);
+        var sum = differenceArr.reduce((a, b) => a + b, 0);
+        //console.log(sum);
+        sumTotal.push(sum);
 
-            for (let i = 0; i < personData.length - 1; i++) {
-                var currentFriendData = personData[i];
-                console.log("last: " + currentFriendData.scores);
-                var friendPhoto = currentFriendData.photo;
-                console.log("photo :" + friendPhoto);
-                var differenceArr = [];
-
-
-
-                for (let j = 0; j < currentFriendData.scores.length; j++) {
-                    var currentFriendScore = currentFriendData.scores[j];
-                    console.log("score: " + currentFriendScore);
-                    console.log("new person score: " + currentScore[j]);
-                    console.log("difference: " + Math.abs(parseInt(currentFriendScore) - parseInt(currentScore[j])));
-                    var difference = Math.abs(parseInt(currentFriendScore) - parseInt(currentScore[j]));
-                    differenceArr.push(difference);
-
-                }
-                console.log(differenceArr);
-                var sum = differenceArr.reduce((a, b) => a + b, 0);
-                console.log(sum);
-                sumTotal.push(sum);
-                var personInfo = {
-                    name: personData[i].name,
-                    photo: friendPhoto,
-                    difference: sum
-                };
-                personArray.push(personInfo);
-
-
-            }
-
-            for (let u = 0; u < personArray.length; u++) {
-                var totalDif = parseInt(personArray[u].difference);
-
-                if (Math.abs(inputSum - totalDif) > 5) {
-                    console.log("difference less than 20: " + personArray[u].name + " photo: " + personArray[u].photo);
-                };
-            }
-
+        for (let u = 0; u < personArray.length; u++) {
+          var totalDif = parseInt(personArray[u].difference);
+          let sub = Math.abs(inputSum - totalDif);
+          //console.log(sub);
+          subArray.push(sub);
         }
-        bestMatch();
+        var personInfo = {
+          name: personData[i].name,
+          photo: friendPhoto,
+          difference: sum
+        };
 
-        console.log(sumTotal);
+        console.log("personInfo.difference: " + personInfo.difference);
+        // let popped = personInfo.difference.pop();
+        // console.log(popped);
 
-        res.json(personData);
-    });
+        console.log(personInfo);
+        personArray.push(personInfo);
+      }
+    };
+    bestMatch();
+
+    console.log("personArray: " + personArray);
+
+    var bubbleSort = function() {
+      console.log("in");
+      for (var i = personArray.length - 1; i >= 0; i--) {
+        console.log("stepped down");
+        console.log(personArray[i].difference);
+        //Number of passes
+        for (var j = personArray.length - i; j > 0; j--) {
+          //Compare the adjacent positions
+          console.log("third step down");
+          console.log(personArray[j].difference);
+          console.log(personArray.difference[j]);
+          if (personArray[j].difference < personArray[j - 1].difference) {
+            //Swap the numbers
+            console.log("into the logic level");
+            var tmp = personArray[j].difference;
+            console.log(temp);
+            personArray.difference[j] = personArray.difference[j - 1];
+            personArray.difference[j - 1] = tmp;
+          }
+        }
+      }
+    };
+    bubbleSort();
+    // console.log(subArray);
+
+    // console.log(sumTotal);
+    res.json(personData);
+  });
 };
